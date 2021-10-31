4798b0a63f3731b7a6b414a69f6a71a7ba343de6
Mike the Tree Climber <mikethetreeclimber@gmail.com>
2021-10-31 18:05:16 -0400

    panel component added

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,13 @@
+<div class="space-y-8">
+  <div class="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+      <div class="px-4 py-5 sm:px-6">
+        {{ $header }}
+      </div>
+      <div class="px-4 py-5 sm:p-6">
+        {{ $slot }}
+      </div>
+      <div class="px-4 py-4 sm:px-6">
+        {{ $footer }}
+      </div>
+    </div>
+</div>
